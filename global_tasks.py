b5c17a6dc42813d3c135ae5f9e2e73287f87ad31
victordunay <80777172+victordunay@users.noreply.github.com>
2021-03-20 14:26:12 +0200

    Update global_tasks.py

--- global_tasks.py
+++ global_tasks.py
@@ -127,14 +127,12 @@ def generate_adversarial_examples_set(model_, results_path_: str, ID_: int, mnis
     # ================================================================
     # generate adversarial examples using all methods
     # ================================================================
-    print("do gd ID=",ID_)
-    adversarial_generator_.generate_projected_gradient_descent_adversarial_examples_set(model_, ID_, mnist_features_,
-                                                                                        mnist_labels_, results_path_)
-    print("do pgd ID=",ID_)
-
-    adversarial_generator_.generate_gradient_descent_adversarial_examples_set(model_, ID_, mnist_features_,
+	print("do gd ID=",ID_)
+	adversarial_generator_.generate_gradient_descent_adversarial_examples_set(model_, ID_, mnist_features_,
                                                                               mnist_labels_, results_path_)
-
+    print("do pgd ID=",ID_)
+	adversarial_generator_.generate_projected_gradient_descent_adversarial_examples_set(model_, ID_, mnist_features_,
+                                                                                        mnist_labels_, results_path_)
     print("do cw ID=",ID_)
 
     adversarial_generator_.generate_carlini_wagner_adversarial_examples_set(model_, ID_, mnist_features_, mnist_labels_,
