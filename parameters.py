51982f8531221ad3b1bc2c37ffd8cfb6aa504080
victordunay <80777172+victordunay@users.noreply.github.com>
2021-03-21 21:20:02 +0200

    Update parameters.py

--- parameters.py
+++ parameters.py
@@ -12,7 +12,7 @@ targeted_labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 # ================================================================
 #  MNIST images IDs for test
 # ================================================================
-image_ids = [1567]
+image_ids = list(range(1,256))
 
 # ================================================================
 #  gradient descent & regularization based attack hyper-parameters
@@ -33,10 +33,10 @@ pgd_rand_vector_size = 4
 #  carlini wagner attack hyper-parameters
 # ================================================================
 cw_rand_vector_size = 2
-cw_lr = [5e-4]  # Todo was   [5e-4, 1e-3, 5e-3]
-cw_search_steps = 2  # Todo was 10
+cw_lr = [5e-4, 1e-3, 5e-3]  # Todo was   [5e-4, 1e-3, 5e-3]
+cw_search_steps = 10  # Todo was 10
 cw_c_range = (1e-3, 1e10)
-cw_max_iter = 10  # Todo was 1000
+cw_max_iter = 1000  # Todo was 1000
 
 # ================================================================
 #  jsma attack hyper-parameters
