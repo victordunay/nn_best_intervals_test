afd5b1daf0a8288a1c5c944709e6328e2b718ace
victordunay <80777172+victordunay@users.noreply.github.com>
2021-04-10 21:05:08 +0300

    Update parameters.py

--- parameters.py
+++ parameters.py
@@ -11,7 +11,7 @@ targeted_labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 # ================================================================
 #  MNIST images IDs for test
 # ================================================================
-image_ids = list(range(1,9))
+image_ids = list(range(9,256))
 
 # ================================================================
 #  gradient descent & regularization based attack hyper-parameters
