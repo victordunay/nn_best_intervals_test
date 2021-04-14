b5fbbae5f7dfe56ce99a6310b93fe00fa44e0e0c
victordunay <80777172+victordunay@users.noreply.github.com>
2021-04-14 19:36:54 +0300

    Update find_best_env.py

--- find_best_env.py
+++ find_best_env.py
@@ -988,8 +988,8 @@ class find_best_env:
             print(" modified pixels=", most_modified_pixels)
             if len(most_modified_pixels) >= max_num_of_modified_pixels:
                 break
-            ind.delete(np.amax(ind))
-            ind.delete(np.amin(ind))
+           np.delete(ind,np.amax(ind))
+           np.delete(ind, np.amix(ind))
 
 
 
