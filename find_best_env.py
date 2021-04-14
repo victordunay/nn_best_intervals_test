3106aecd017276e56a71d17557010c394ae72b0c
victordunay <80777172+victordunay@users.noreply.github.com>
2021-04-14 19:58:05 +0300

    Update find_best_env.py

--- find_best_env.py
+++ find_best_env.py
@@ -985,7 +985,7 @@ class find_best_env:
                 if ind[i] == np.amax(ind) or ind[i] == np.amin(ind):
                     print("idx i =", i, "ind [i]=", ind[i])
                     most_modified_pixels.append(i)
-                    ind[i] = None
+                    ind[i] = (np.amax(ind)+np.amin(ind))/2
             print(" modified pixels=", most_modified_pixels)
             if len(most_modified_pixels) >= max_num_of_modified_pixels:
                 break
