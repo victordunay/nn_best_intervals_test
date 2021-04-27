baa9cdf6dc742f227e0442f978abc977ad0a5cc1
victordunay <80777172+victordunay@users.noreply.github.com>
2021-04-27 18:55:54 +0300

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -26,22 +26,23 @@ def parallel_process(model_, results_path_: str, ID_: int, mnist_features_, mnis
     # ================================================================
     #  generate adversarial examples
     # ================================================================
-    global_tasks.generate_adversarial_examples_set(model_, results_path_, ID_, mnist_features_, mnist_labels_,adversarial_generator_)
+    global_tasks.generate_adversarial_examples_set(model_, results_path_, ID_, mnist_features_, mnist_labels_,
+                                                   adversarial_generator_)
     # ================================================================
     # calculate mean vector between all adversarial attack methods
     # ================================================================
-    #adversarial_examples_set = global_tasks.calculate_mean(results_path_, ID_, image_size)
+    # adversarial_examples_set = global_tasks.calculate_mean(results_path_, ID_, image_size)
 
     # ================================================================
     # view adversarial process results
     # ================================================================
-    #global_tasks.view_adversarial_results(ID_, results_path, mnist_features)
+    # global_tasks.view_adversarial_results(ID_, results_path, mnist_features)
 
     # ================================================================
     # find maximum environment
     # ================================================================
 
-    #interval_solver.find_max_intervals(results_path, ID_, mnist_features, mnist_labels)
+    # interval_solver.find_max_intervals(results_path, ID_, mnist_features, mnist_labels)
 
     return ID_
 
@@ -81,18 +82,17 @@ if __name__ == "__main__":
     #  interval_solver instantiation
     # ================================================================
     interval_solver = find_best_env.find_best_env(parameters.search_params)
-   
+
     processes = [mp.Process(target=parallel_process, args=(
-    model, results_path, ID, mnist_features, mnist_labels, adversarial_generator, parameters.image_size)) for ID in
-                  parameters.image_ids]
-     for p in processes:
+        model, results_path, ID, mnist_features, mnist_labels, adversarial_generator, parameters.image_size)) for ID in
+                 parameters.image_ids]
+    for p in processes:
         p.start()
-     for p in processes:
-         p.join()
-   
-   
+    for p in processes:
+        p.join()
+
     #    for ID in parameters.image_ids:
-            # ================================================================
+    # ================================================================
     #         #  generate adversarial examples
     #         # ================================================================
     #         #global_tasks.generate_adversarial_examples_set(model, results_path, ID, mnist_features, mnist_labels,adversarial_generator)
@@ -108,9 +108,7 @@ if __name__ == "__main__":
     #         #interval_solver.view_results_single_pix_l0_line_graph(ID)
     #         interval_solver.test_multiple_epsilon_inf(ID, mnist_features, mnist_labels)
     #
-    
 
-  
     # # results = [pool.apply(parallel_process, args=(model,results_path,ID,mnist_features,mnist_labels,adversarial_generator,parameters.image_size)) for ID in parameters.image_ids]
     #
     #
@@ -136,33 +134,31 @@ if __name__ == "__main__":
     # # interval_solver.find_max_intervals(adversarial_examples_set, ID, mnist_features, mnist_labels)================
     #  load pre-trained model parameters into model
     # ================================================================
-    #model.load_state_dict(torch.load(neural_network_path))
+    # model.load_state_dict(torch.load(neural_network_path))
     # ================================================================
     #  adversarial_generator instantiation
     # ================================================================
-    #adversarial_generator = attack_models.attacks(parameters.attack_params)
+    # adversarial_generator = attack_models.attacks(parameters.attack_params)
     # ================================================================
     #  interval_solver instantiation
     # ================================================================
-    #interval_solver = find_best_env.find_best_env(parameters.search_params)
+    # interval_solver = find_best_env.find_best_env(parameters.search_params)
     # for ID in parameters.image_ids:
-        # ================================================================
-        #  generate adversarial examples
-        # ================================================================
-        #global_tasks.generate_adversarial_examples_set(model, results_path, ID, mnist_features, mnist_labels,adversarial_generator)
-        #global_tasks.view_adversarial_results(ID, results_path, mnist_features)
-        #interval_solver.find_max_intervals(results_path, ID, mnist_features, mnist_labels)
-        #interval_solver.show_intervals(ID,results_path, mnist_features, mnist_labels)
-        #interval_solver.show_hist_final(ID,results_path)
-        #interval_solver.calculate_epsilon_inf(ID, mnist_features, mnist_labels)
+    # ================================================================
+    #  generate adversarial examples
+    # ================================================================
+    # global_tasks.generate_adversarial_examples_set(model, results_path, ID, mnist_features, mnist_labels,adversarial_generator)
+    # global_tasks.view_adversarial_results(ID, results_path, mnist_features)
+    # interval_solver.find_max_intervals(results_path, ID, mnist_features, mnist_labels)
+    # interval_solver.show_intervals(ID,results_path, mnist_features, mnist_labels)
+    # interval_solver.show_hist_final(ID,results_path)
+    # interval_solver.calculate_epsilon_inf(ID, mnist_features, mnist_labels)
 
-        #interval_solver.view_most_modified_pixels(results_path, ID)
-        #interval_solver.test_single_pix_l0(results_path, ID,mnist_features, mnist_labels)
-        #interval_solver.view_results_single_pix_l0(ID)
-        #interval_solver.view_results_single_pix_l0_line_graph(ID)
-        #print("results=",np.mean(np.load('epsilon_intervals'+ 'mean_total_result' + str(ID)+'.npy')))
-
-
+    # interval_solver.view_most_modified_pixels(results_path, ID)
+    # interval_solver.test_single_pix_l0(results_path, ID,mnist_features, mnist_labels)
+    # interval_solver.view_results_single_pix_l0(ID)
+    # interval_solver.view_results_single_pix_l0_line_graph(ID)
+    # print("results=",np.mean(np.load('epsilon_intervals'+ 'mean_total_result' + str(ID)+'.npy')))
 
 # results = [pool.apply(parallel_process, args=(model,results_path,ID,mnist_features,mnist_labels,adversarial_generator,parameters.image_size)) for ID in parameters.image_ids]
 
