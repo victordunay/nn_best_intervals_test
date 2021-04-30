8570b9306acc04c82ec24fa154a10b6d915d2d0a
victordunay <80777172+victordunay@users.noreply.github.com>
2021-04-30 18:28:32 +0300

    Update attack_models.py

--- attack_models.py
+++ attack_models.py
@@ -58,6 +58,7 @@ class attacks:
                 adversarial_goal = torch.tensor([t])
                 lam = torch.tensor([reg_factor])
                 for i in range(self.gd_max_iter):
+                    print("iter=",i)
                     manual_prediction = net(adv_example.reshape(1, 1, 28, 28))
 
                     _, predicted = torch.max(manual_prediction.data, 1)
