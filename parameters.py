38365d3392d3acb45162ec0e3f5830244e25c339
victordunay <80777172+victordunay@users.noreply.github.com>
2021-05-09 17:49:55 +0300

    Update parameters.py

--- parameters.py
+++ parameters.py
@@ -60,10 +60,9 @@ attack_params = dict(targeted_labels=targeted_labels, jsma_lr=jsma_lr, jsma_max_
 #  search algorithm hyper-parameters
 # ================================================================
 
-neural_network = 'conv_small'  ## Todo 'relu_3_100_mnist'
+neural_network = 'mnist_conv_maxpool'  ## Todo 'relu_3_100_mnist'
 eran_domain = 'deepzono'
-#model_path = './models/' + neural_network + '.tf'
-model_path = './models/' + 'convSmallRELU__Point.onnx'
+model_path = './models/' + neural_network + '.tf'
 
 # intervals_path = '/home/eran/Desktop/epsilon_intervals'
 intervals_path = 'epsilon_intervals'
