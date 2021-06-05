19a8ee6101a80e74fb07ad9600176c9c7f10601c
victordunay <80777172+victordunay@users.noreply.github.com>
2021-06-05 20:50:38 +0300

    Update find_best_env.py

--- find_best_env.py
+++ find_best_env.py
@@ -1220,7 +1220,7 @@ class find_best_env:
         print("test_time.shape=", test_time.shape)
         print("M.shape=", M.shape)
 
-        plt.plot(pixel_time,linesytle='dotted')
+        plt.plot(pixel_time)
         plt.savefig(results_path + '/pixel_time.png')
         print("pixel_time.mean=",pixel_time.mean())
         print("pixel_time.var=",pixel_time.var())
