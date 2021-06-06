58e49d87b5e29f405c5292aa6f751e19ad5d5605
victordunay <80777172+victordunay@users.noreply.github.com>
2021-06-06 19:05:47 +0300

    Update find_best_env.py

--- find_best_env.py
+++ find_best_env.py
@@ -1247,7 +1247,7 @@ class find_best_env:
 
         x_valid= []
         x_invalid = []
-        for i in range(300):
+        for i in range(verified_results.shape[0]):
             if verified_results[i] ==1:
                 x_valid.append(i)
             else:
@@ -1256,8 +1256,8 @@ class find_best_env:
         plt.title("pixel test")
         plt.xlabel('test idx')
         plt.ylabel('environment size')
-        plt.plot(x_invalid, M[x_invalid], color="red", marker='D', mfc='red', linewidth=0.05, markersize=4)
-        plt.plot(x_valid, M[x_valid], color="green", marker='D', mfc='green', linewidth=0.2, markersize=4)
+        plt.plot(x_invalid, M[x_invalid], color="red", marker='D', mfc='red', linewidth=0.01, markersize=0.1)
+        plt.plot(x_valid, M[x_valid], color="green", marker='D', mfc='green', linewidth=0.03, markersize=0.1)
         plt.legend(["VALD RESULT", "INVALID RESULT"])
 
         plt.show()
