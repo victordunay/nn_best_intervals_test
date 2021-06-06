286881b0479df355b145cd79982179b1805f5719
victordunay <80777172+victordunay@users.noreply.github.com>
2021-06-06 18:41:42 +0300

    Update find_best_env.py

--- find_best_env.py
+++ find_best_env.py
@@ -1256,7 +1256,7 @@ class find_best_env:
         plt.title("pixel test")
         plt.xlabel('test idx')
         plt.ylabel('pixel environment')
-        plt.plot(x_invalid, M[x_invalid], color="red", marker='D', mfc='red', linewidth=0.05, markersize=0.05)
+        #plt.plot(x_invalid, M[x_invalid], color="red", marker='D', mfc='red', linewidth=0.05, markersize=0.05)
         plt.plot(x_valid, M[x_valid], color="green", marker='D', mfc='green', linewidth=0.5, markersize=0.5)
         plt.legend(["INVALD TEST", "VALID TEST"])
 
